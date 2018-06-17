f36264a8254d5e4dd5b5f0c5a318115e420bae63
Violeta <morada.libertad@gmail.com>
2018-06-16 20:20:19 -0400

    HTML y CSS casi listo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+// DOM
 const lima = document.getElementById('lima');
-lima.addEventListener('click', () => {
-  console.log('HOLA');
-});
+const santiago = document.getElementById('santiago');
+const mexico = document.getElementById('mexico');
+const brasil = document.getElementById('brasil');
+
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
