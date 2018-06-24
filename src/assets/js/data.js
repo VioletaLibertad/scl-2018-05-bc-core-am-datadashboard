0277f608c69f25a10d8697fc7c948e4fadd25101
Violeta <morada.libertad@gmail.com>
2018-06-24 17:34:17 -0400

    rutas arregladas, carpetas ordenadas

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,8 +1,8 @@
 window.onload = () => {
   const limaBtn = document.getElementById('lima-users'); // Btn que activa todo
-  const usersJson = '../data/cohorts/lim-2018-03-pre-core-pw/users.json'; // Datos de usuarios
-  const progressJson = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; // Datos de progreso de usuarios
-  const cohortsJson = '../data/cohorts.json'; // Datos de cohorts
+  const usersJson = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json'; // Datos de usuarios
+  const progressJson = '../../../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; // Datos de progreso de usuarios
+  const cohortsJson = '../../../data/cohorts.json'; // Datos de cohorts
   const limaCourses = document.getElementById('lima-courses');
   let users = [];
   let progress = [];
@@ -30,26 +30,22 @@ window.onload = () => {
       users.push(response[0]);
       progress.push(response[1]);
       response[2].forEach(data => {
-        courses.push(data.coursesIndex);
-        // console.log(courses);
+        let coursesId = JSON.stringify(data.coursesIndex);
+        courses.push(coursesId);
       });
+      console.log(users, progress, courses);
       return users, progress, courses;
     }).catch((error) => {
       console.log('Hay un error: ' + error);
     });
   }
 
-<<<<<<< HEAD:src/assets/js/data.js
   limaCourses.addEventListener('click', () => {
     computeUsersStats();
   });
 
   window.computeUsersStats = (users, progress, courses) => {
-    for (let i = 0; i < users.length; i++) {
-      let userID = users[i].id;
-      let progressUsers = progress[userID];
-      console.log(progressUsers);
-    }
+    
     /*
     class Stats {
       constructor(percent, exercises, reads, quizzes) {
@@ -71,7 +67,3 @@ window.sortUsers = (users, orderBy, orderDirection) => {
 window.filterUsers = (users, search) => {
 
 };
-=======
-}
-
->>>>>>> 9bf6bb98ae88d68b197ffde30cab6028c572b740:src/main.js
